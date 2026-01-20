9c03db3fa07f7e31c7922c6904517cfc14f3aa62
user2122-ay <anthonybatista2010@gmail.com>
2026-01-20 18:29:27 -0500

    Update DNISchema to make discordId optional
    Removed 'required' constraint from discordId field.

--- models/DNI.js
+++ models/DNI.js
@@ -1,13 +1,13 @@
 const { Schema, model } = require('mongoose');
 
 const DNISchema = new Schema({
-  discordId: { type: String, required: true, unique: true },
+  discordId: { type: String, unique: true },
   nombreIC: String,
   apellidoIC: String,
   edadIC: Number,
   fechaNacimiento: String,
   tipoSangre: String,
   numeroDNI: String
-}, { timestamps: true });
+});
 
 module.exports = model('DNI', DNISchema);
