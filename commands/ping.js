104d49dc8a2bd09c1342ffbb466ab6d6794bfff2
user2122-ay <anthonybatista2010@gmail.com>
2026-01-24 11:03:30 -0500

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Comando de prueba"),
+
+  async execute(interaction) {
+    await interaction.reply("🏓 Pong! El bot está vivo.");
+  }
+};
