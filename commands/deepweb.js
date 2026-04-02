f7316de18cdbc811eb706495ed20b1f391e0b6ba
user2122-ay <anthonybatista2010@gmail.com>
2026-04-02 16:02:20 -0500

    Add permissions property to deepweb command

--- commands/deepweb.js
+++ commands/deepweb.js
@@ -5,6 +5,8 @@ const CANAL_DEEPWEB = "1465098377120452628";
 const LOGO_URL = "https://cdn.discordapp.com/attachments/1463192290469085257/1465099204182413353/images_1769377256855.jpg";
 
 module.exports = {
+  permisos: "🌐 Todos", // 👈 AÑADIDO
+
   data: new SlashCommandBuilder()
     .setName("deepweb")
     .setDescription("Enviar un mensaje anónimo a la Deep Web")
