21f58387eb97f9cb11977e4869ca101ff6c76435
user2122-ay <anthonybatista2010@gmail.com>
2026-04-14 22:29:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ require("./events/seguridad")(client);
 require("./events/logs")(client);
 require("./events/bienvenida")(client);
 require("./events/presence")(client);
-require("./events/dmSystem")(client);
+
 
 // ==============================
 // 📦 COMANDOS
