554d6df78a4860449006a8081a779f88edb9efab
user2122-ay <anthonybatista2010@gmail.com>
2026-04-25 22:06:05 -0500

    Add AFK event handler to index.js

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ require("./events/bienvenida")(client);
 require("./events/presence")(client);
 require("./events/mencionBot")(client);
 require("./events/tickets")(client); 
+require("./events/afk")(client);
 
 // ==============================
 // 📦 COMANDOS
