adb797153858294d1725ab4840c58160cf10244a
user2122-ay <anthonybatista2010@gmail.com>
2026-05-05 08:07:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ require("./events/bienvenida")(client);
 require("./events/presence")(client);
 require("./events/mencionBot")(client);
 require("./events/tickets")(client);
-require("./events/verificacion")(client);
+
 
 // ==============================
 // 📦 COMANDOS
