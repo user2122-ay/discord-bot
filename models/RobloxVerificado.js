09e456ab5494613eed6eaeb53d9055d09d62f8c7
user2122-ay <anthonybatista2010@gmail.com>
2026-05-26 16:22:45 -0500

    Create RobloxVerificado model with schema

---
+++ models/RobloxVerificado.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const RobloxVerificadoSchema = new mongoose.Schema({
+
+  discordId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  robloxId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  robloxUser: {
+    type: String,
+    required: true
+  },
+
+  fecha: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+
+module.exports = mongoose.model(
+  "RobloxVerificado",
+  RobloxVerificadoSchema
+);
